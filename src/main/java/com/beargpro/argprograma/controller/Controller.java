475478af2b1e4aed7d4e7c3cc671e0dd4041d98d
Dighitalsoluciones <hernanglomba@gmail.com>
2022-08-20 23:31:43 -0300

    agregado de crossorigin solucion a cors

--- src/main/java/com/beargpro/argprograma/controller/Controller.java
+++ src/main/java/com/beargpro/argprograma/controller/Controller.java
@@ -1,10 +1,12 @@
 
 package com.beargpro.argprograma.controller;
 
+
 import com.beargpro.argprograma.model.Encabezado;
 import com.beargpro.argprograma.service.IEncabezadoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class Controller {
     @Autowired
