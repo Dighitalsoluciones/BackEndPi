432415960862bd245874530b2fb116bf1ec6320e
Dighitalsoluciones <hernanglomba@gmail.com>
2022-08-22 11:06:28 -0300

    agregado origins correcto

--- src/main/java/com/beargpro/argprograma/controller/Controller.java
+++ src/main/java/com/beargpro/argprograma/controller/Controller.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
     @Autowired
     private IEncabezadoService encaServ;
